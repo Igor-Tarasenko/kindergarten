fd85f2daff148ec7c817645a994ad0fcb9ce8993
Igor Tarasenko <tarasenko.igr@gmail.com>
2018-11-30 15:26:43 +0200

    add js for select

--- build/js/main.js
+++ build/js/main.js
@@ -14,6 +14,29 @@ $(document).ready(function() {
             }
         });
     }
+    var selectArray = {};
+    $('.first-select-block-js .jq-selectbox__dropdown').click(function (e) {
+        e.preventDefault();
+        var value = $('.first-select-block-js select').val();
+        selectArray.select1 = value;
+        var select1Lenth = selectArray.select1.length;
+        console.log(selectArray);
+        console.log(select1Lenth);
+    });
+    $('.second-select-block-js .jq-selectbox__dropdown').click(function (e) {
+        e.preventDefault();
+        var value2 = $('.second-select-block-js select').val();
+        selectArray.select2 = value2;
+        // if (selectArray.length)
+        console.log(selectArray);
+    });
+    $('.third-select-block-js .jq-selectbox__dropdown').click(function (e) {
+        e.preventDefault();
+        var value3 = $('.third-select-block-js select').val();
+        selectArray.select3 = value3;
+        var ajaksLine = JSON.stringify(selectArray);
+        console.log(ajaksLine);
+    });
     $('.specifications').click(function () {
         $('.specifications, .schedule, .reviews').css({'borderTop' : '14px solid transparent'}).find('button').css({'borderTopLeftRadius' : '15px'});
         $('.specifications').css('border-top', '14px solid #fe6e1f').find('button').css({'borderTopLeftRadius' : '0'});
